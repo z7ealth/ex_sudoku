a114495bb0e1cb5b2d7595d36c77cae0bc510ef4
Héctor Salinas <hector_sag@outlook.com>
2023-12-29 12:52:45 -0600

    Created test cases, optimized functions

--- lib/ex_sudoku.ex
+++ lib/ex_sudoku.ex
@@ -13,20 +13,19 @@ defmodule ExSudoku do
     units = get_units(squares, unit_list)
     peers = get_peers(units)
 
-    dbg(peers["C2"])
-
     %{
       squares: squares,
       unit_list: unit_list,
-      units: units
+      units: units,
+      peers: peers
     }
   end
 
   defp cross(a, b), do: for(x <- a, y <- b, do: <<x, y>>)
 
   defp get_unit_list do
-    first = for c <- @cols, do: cross(@rows, [c])
-    second = for r <- @rows, do: cross([r], @cols)
+    first = Enum.map(@cols, &cross(@rows, [&1]))
+    second = Enum.map(@rows, &cross([&1], @cols))
 
     third =
       for rs <- ~w(ABC DEF GHI),
@@ -36,23 +35,20 @@ defmodule ExSudoku do
     first ++ second ++ third
   end
 
-  defp get_units(squares, unit_list),
-    do: for(s <- squares, into: %{}, do: {s, Enum.filter(unit_list, fn u -> s in u end)})
+  defp get_units(squares, unit_list) do
+    squares
+    |> Enum.map(&{&1, Enum.filter(unit_list, fn unit -> &1 in unit end)})
+    |> Enum.into(%{})
+  end
 
   defp get_peers(units) do
-    for {k, v} <- units, into: %{} do
-      new_units =
-        for item <- v, into: [] do
-          List.delete(item, k)
-        end
-
-      unique_units = Enum.concat(new_units) |> Enum.uniq()
-
-      new_units = for item <- new_units, into: [] do
-        Enum.slice(item, unique_units)
-      end
-
-      {k, new_units}
-    end
+    Enum.map(units, fn {k, unit} ->
+      unit
+      |> List.flatten()
+      |> Enum.uniq()
+      |> List.delete(k)
+      |> (&{k, &1}).()
+    end)
+    |> Enum.into(%{})
   end
 end
--- test/ex_sudoku_test.exs
+++ test/ex_sudoku_test.exs
@@ -1,15 +1,61 @@
 defmodule ExSudokuTest do
   use ExUnit.Case
   doctest ExSudoku
-  
-  @data ExSudoku.run()
 
+  setup_all do
+    data = ExSudoku.run()
 
-  test "Sudoku squares" do
-    assert length(@data.squares) == 81
+    IO.inspect(data, label: "Testing with data")
+
+    {:ok, data: data}
   end
 
-  test "Sudoku unit_list" do
-    assert length(@data.unit_list) == 27
+  test "Sudoku squares", context do
+    assert length(context[:data].squares) == 81
+  end
+
+  test "Sudoku unit_list", context do
+    assert length(context[:data].unit_list) == 27
+  end
+
+  test "Sudoku unit length", context do
+    Enum.each(context[:data].units, fn {_, v} -> assert length(v) == 3 end)
+  end
+
+  test "Sudoku peer length", context do
+    Enum.each(context[:data].peers, fn {_, v} -> assert length(v) == 20 end)
+  end
+
+  test "Sudoku units", context do
+    assert context[:data].units["C2"] == [
+             ["A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2", "I2"],
+             ["C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9"],
+             ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]
+           ]
+  end
+
+  test "Sudoku peers", context do
+    assert context[:data].peers["C2"] == [
+             "A2",
+             "B2",
+             "D2",
+             "E2",
+             "F2",
+             "G2",
+             "H2",
+             "I2",
+             "C1",
+             "C3",
+             "C4",
+             "C5",
+             "C6",
+             "C7",
+             "C8",
+             "C9",
+             "A1",
+             "A3",
+             "B1",
+             "B3"
+           ]
   end
 end
